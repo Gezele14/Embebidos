2212733e96cefbaa6b0b90fe0a4a127c121ffd9a
Gerardo Zeledon <gerardo.zeledon14@gmail.com>
2020-02-21 12:23:07 -0600

    Add Taller 1 and 2

---
+++ Taller 3/mathC.h
@@ -0,0 +1,10 @@
+#ifndef _MATH_H
+#define _MATH_H
+
+float add(float num1, float num2);
+float sub(float num1, float num2);
+float mul(float num1, float num2);
+float div(float num1, float num2);
+float raiz(float num1);
+
+#endif
